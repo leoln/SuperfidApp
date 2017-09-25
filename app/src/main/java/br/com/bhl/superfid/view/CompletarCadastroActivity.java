9923bbbea91eb9eacb2996d011534ec40d5f6f2b
hericlespontes <hericleshg@yahoo.com.br>
2017-09-24 23:40:06 -0300

    Bluetooth e outros org

--- app/src/main/java/br/com/bhl/superfid/view/CompletarCadastroActivity.java
+++ app/src/main/java/br/com/bhl/superfid/view/CompletarCadastroActivity.java
@@ -1,7 +1,9 @@
-package br.com.bhl.superfid;
+package br.com.bhl.superfid.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.bhl.superfid.R;
 
 public class CompletarCadastroActivity extends AppCompatActivity {
 
