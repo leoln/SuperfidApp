6bafba2971e02f76d8cd2ea778fe15904bf2f72d
hericlespontes <hericleshg@yahoo.com.br>
2017-10-15 21:02:04 -0200

    commit hericles

--- app/src/main/java/br/com/bhl/superfid/controller/ComprasAdapter.java
+++ app/src/main/java/br/com/bhl/superfid/controller/ComprasAdapter.java
@@ -1,11 +1,13 @@
 package br.com.bhl.superfid.controller;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.ByteArrayInputStream;
 import java.util.List;
 
 import br.com.bhl.superfid.R;
@@ -42,9 +44,9 @@ public class ComprasAdapter extends RecyclerView.Adapter {
 
         holder.descricao.setText(itemCarrinho.getProduto().getDescricao());
         holder.precoUnitario.setText("" + itemCarrinho.getProduto().getPrecoUnitario());
-        holder.unidades.setText("" + itemCarrinho.getProduto().getUnidade());
         holder.validade.setText("" + itemCarrinho.getProduto().getDataValidade());
         holder.quantidade.setText(""+ itemCarrinho.getQuantidade());
+
     }
 
     @Override
--- app/src/main/java/br/com/bhl/superfid/controller/ComprasViewHolder.java
+++ app/src/main/java/br/com/bhl/superfid/controller/ComprasViewHolder.java
@@ -2,6 +2,7 @@ package br.com.bhl.superfid.controller;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import br.com.bhl.superfid.R;
@@ -9,7 +10,6 @@ public class ComprasViewHolder extends RecyclerView.ViewHolder {
     final TextView descricao;
     final TextView precoUnitario;
     final TextView validade;
-    final TextView unidades;
     final TextView quantidade;
 
     public ComprasViewHolder(View view) {
@@ -17,7 +17,6 @@ public class ComprasViewHolder extends RecyclerView.ViewHolder {
         descricao = (TextView) view.findViewById(R.id.item_produto_descricao);
         precoUnitario = (TextView) view.findViewById(R.id.item_produto_precoUnitario);
         validade = (TextView) view.findViewById(R.id.item_produto_validade);
-        unidades = (TextView) view.findViewById(R.id.item_produto_unidades);
-        quantidade = (TextView) view.findViewById(R.id.quantidade);
+        quantidade = (TextView) view.findViewById(R.id.item_carrinho_quantidade);
     }
 }
